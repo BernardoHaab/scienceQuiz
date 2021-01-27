1612e196948e9ed2997f6992c6607ad2b24b1022
BernardoHaab <bernardohaab@gmail.com>
2021-01-27 12:15:41 -0300

    create introduction page structure
    Signed-off-by: BernardoHaab <bernardohaab@gmail.com>

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,35 @@
-import styled from 'styled-components'
+import styled from "styled-components";
+import db from "../db";
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+import Widget from "../src/components/Widget";
+import Footer from "../src/components/Footer";
+import GitHubCorner from "../src/components/GitHubCorner";
+import QuizContainer from "../src/components/QuizContainer";
+import QuizBackground from "../src/components/QuizBackground";
 
 export default function Home() {
-  return <Title>My page</Title>
+    return (
+        <QuizBackground backgroundImage={db.bg}>
+            <QuizContainer>
+                <Widget>
+                    <Widget.Header>
+                        <h1>O quiz da Ciência</h1>
+                    </Widget.Header>
+                    <Widget.Content>
+
+                        <p>Lorem ipsum dolor sit amet, consectetur adip</p>
+                    </Widget.Content>
+                </Widget>
+                <Widget>
+                    <Widget.Content>
+                        <h1>Quizes da galera</h1>
+
+                        <p>Lorem ipsum dolor sit amet, consectetur adip</p>
+                    </Widget.Content>
+                </Widget>
+                <Footer/>
+            </QuizContainer>
+            <GitHubCorner/>
+        </QuizBackground>
+    );
 }
